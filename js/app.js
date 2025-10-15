6de69c14790663c693addbb7a7033e1cf062269d
Dardan <dardan.has@gmail.com>
2025-10-15 21:24:30 +0200

    Added comments

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+const text = document.getElementById("maintext");
+const button = document.getElementById("knapp");
+
+button.addEventListener("click", function() {
+text.innerText = "Du klickade på knappen";
+});
