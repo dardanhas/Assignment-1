c34e9c9fe5bf70d416faa9bf1e6886ebaf4b2ba2
Dardan <dardan.has@gmail.com>
2025-10-16 09:33:17 +0200

    Updated js. comments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
-
+//Javascript och HTML samarbetar genom att hämta element via deras ID (getElementByID).
+//Här hämtar texten <p> och knappen <button> från HTML.
 const text = document.getElementById("maintext");
 const button = document.getElementById("knapp");
 
+//När användaren klickar på knappen, ändrad JavaScript textinnehållet i <p> elementet.
 button.addEventListener("click", function() {
 text.innerText = "Du klickade på knappen";
 });
